ceafae1da5fdc7ac1ec45a79a9affea18329ab34
solletiharshitha <solletiharshitha@gmail.com>
2021-08-01 23:41:22 +0530

    [Harshitha] Delete data in table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,5 +1,6 @@
 ----------------UC1:Create Database----------------
 create database AddressBookService;
+
 ----------------UC2:Create Table----------------
 create table AddressBookTable
 (
@@ -12,10 +13,15 @@ zip BigInt,
 PhoneNumber BigInt,
 Email varchar(200)
 )
+
 ------ UC3:Insert Values into Table ------
 Insert into AddressBookTable(FirstName,LastName,Address,City,State,zip,PhoneNumber,Email) 
 values('Arjun','A','Nellore Road','Pamur','AndhraPradesh',523108,8858548393,'arjun@gmail.com'),
 ('Vanitha','P','Gandhi Nagar','Ongole','AndhraPradesh',523001,9995559955,'vanitha@gamil.com'),
 ('Sameera','S','Magunta Layout','Nellore','AndhraPradesh',524003,8393839475,'sameera@gmail.com');
+
 ------ UC4:Update Values in Table ------
 update AddressBookTable set Email='vani123@gmail.com' where FirstName='Vanitha';
+
+------ UC5:Delete Values in Table ------
+delete AddressBookTable where FirstName='Vanitha';
